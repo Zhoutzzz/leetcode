a881d103e884cebaa18199737f322c72688e50ec
Zhoutzzz <925920668@qq.com>
2022-07-04 21:23:17 +0800

    solution

---
+++ solution/MinimumAbsoluteDifference_Lt1200.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"math"
+	"sort"
+)
+
+func minimumAbsDifference(arr []int) (res [][]int) {
+	arrLen := len(arr)
+	sort.Ints(arr)
+
+	for i, abs := 0, math.MaxInt; i < arrLen-1; i++ {
+		if curAbs := arr[i+1] - arr[i]; curAbs < abs {
+			abs = curAbs
+			res = [][]int{{arr[i], arr[i+1]}}
+		} else if curAbs == abs {
+			res = append(res, []int{arr[i], arr[i+1]})
+
+		}
+	}
+
+	return res
+}
