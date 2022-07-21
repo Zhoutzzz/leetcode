8a27bd11970ed5616a9f960e8e71c8b07597cabf
Zhoutzzz <925920668@qq.com>
2022-07-21 13:51:58 +0800

    solution

---
+++ solution/medium/BinaryTreePruning_Lt814.go
@@ -0,0 +1,24 @@
+package medium
+
+import "leetcode/solution/common"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func pruneTree(root *common.TreeNode) *common.TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left = pruneTree(root.Left)
+	root.Right = pruneTree(root.Right)
+	if root.Val == 0 && root.Right == nil && root.Left == nil {
+		root = nil
+	}
+	return root
+}
