5a4e8d8c7d09bb2a1f67b0ea1486285e219d8dd7
Zhoutzzz <925920668@qq.com>
2022-08-25 21:46:44 +0800

    solution

---
+++ solution/medium/FindKClosestElements_Lt658.go
@@ -0,0 +1,29 @@
+package medium
+
+import (
+	"sort"
+)
+
+func findClosestElements(arr []int, k int, x int) []int {
+	l := len(arr)
+	j := sort.SearchInts(arr, x)
+	i := j - 1
+	for ; k > 0; k-- {
+		if i < 0 {
+			j++
+		} else if j >= l || x-arr[i] <= arr[j]-x {
+			i--
+		} else {
+			j++
+		}
+	}
+	return arr[i+1 : j]
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	} else {
+		return x
+	}
+}
