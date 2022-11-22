74cb31859e70dfbd986a883b598775ccd7e2cc33
Zhoutzzz <925920668@qq.com>
2022-11-22 22:49:56 +0800

    solution

---
+++ solution/hard/NO.NMericalNumber_Lt878.go
@@ -0,0 +1,33 @@
+package hard
+
+const mod int = 1e9 + 7
+
+func nthMagicalNumber(n, a, b int) int {
+	l := min(a, b)
+	r := n * min(a, b)
+	c := a / gcd(a, b) * b
+	for l <= r {
+		mid := (l + r) / 2
+		cnt := mid/a + mid/b - mid/c
+		if cnt >= n {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return (r + 1) % mod
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func gcd(a, b int) int {
+	if b != 0 {
+		return gcd(b, a%b)
+	}
+	return a
+}
