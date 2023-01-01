31c39a471b0a3d37d455b3f82466b394fc8922fe
Zhoutzzz <925920668@qq.com>
2023-01-01 16:55:17 +0800

    solution

---
+++ solution/easy/FirstLetter_Lt2351.go
@@ -0,0 +1,12 @@
+package easy
+
+func repeatedCharacter(s string) byte {
+	seen := map[rune]bool{}
+	for _, c := range s {
+		if seen[c] {
+			return byte(c)
+		}
+		seen[c] = true
+	}
+	return 0 // impossible
+}
