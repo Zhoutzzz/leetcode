44afe52d324972e4415a77f08e47891bbc3e4117
Zhoutzzz <925920668@qq.com>
2023-01-06 19:38:38 +0800

    solution

---
+++ solution/easy/CountIntegersWithEven_Lt2180.go
@@ -0,0 +1,14 @@
+package easy
+
+func countEven(num int) (ans int) {
+	for i := 1; i <= num; i++ {
+		sum := 0
+		for x := i; x > 0; x /= 10 {
+			sum += x % 10
+		}
+		if sum%2 == 0 {
+			ans++
+		}
+	}
+	return
+}
