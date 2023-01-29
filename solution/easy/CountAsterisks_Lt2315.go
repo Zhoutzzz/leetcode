2fbc69faa72504c4ac1bcc3b238245b5d96f2b52
Zhoutzzz <925920668@qq.com>
2023-01-29 23:46:58 +0800

    solution

---
+++ solution/easy/CountAsterisks_Lt2315.go
@@ -0,0 +1,13 @@
+package easy
+
+func countAsterisks(s string) (res int) {
+	valid := true
+	for _, c := range s {
+		if c == '|' {
+			valid = !valid
+		} else if c == '*' && valid {
+			res++
+		}
+	}
+	return
+}
