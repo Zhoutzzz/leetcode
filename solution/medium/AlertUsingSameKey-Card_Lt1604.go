45debf83e7a55a8cc6a1f5a1346781d1ddc4fc34
Zhoutzzz <925920668@qq.com>
2023-02-07 12:49:32 +0800

    solution

---
+++ solution/medium/AlertUsingSameKey-Card_Lt1604.go
@@ -0,0 +1,24 @@
+package medium
+
+import "sort"
+
+func alertNames(keyName, keyTime []string) (ans []string) {
+	timeMap := map[string][]int{}
+	for i, name := range keyName {
+		t := keyTime[i]
+		hour := int(t[0]-'0')*10 + int(t[1]-'0')
+		minute := int(t[3]-'0')*10 + int(t[4]-'0')
+		timeMap[name] = append(timeMap[name], hour*60+minute)
+	}
+	for name, times := range timeMap {
+		sort.Ints(times)
+		for i, t := range times[2:] {
+			if t-times[i] <= 60 {
+				ans = append(ans, name)
+				break
+			}
+		}
+	}
+	sort.Strings(ans)
+	return
+}
