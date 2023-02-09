f6f5bd42dfba4fa4ae01231a0febdd4f12b99623
Zhoutzzz <925920668@qq.com>
2023-02-09 20:47:18 +0800

    solution

---
+++ solution/medium/DesignAuthenticationManager_Lt1797.go
@@ -0,0 +1,29 @@
+package medium
+
+type AuthenticationManager struct {
+	mp  map[string]int
+	ttl int
+}
+
+func Constructor1797(timeToLive int) AuthenticationManager {
+	return AuthenticationManager{map[string]int{}, timeToLive}
+}
+
+func (m *AuthenticationManager) Generate(tokenId string, currentTime int) {
+	m.mp[tokenId] = currentTime
+}
+
+func (m *AuthenticationManager) Renew(tokenId string, currentTime int) {
+	if v, ok := m.mp[tokenId]; ok && v+m.ttl > currentTime {
+		m.mp[tokenId] = currentTime
+	}
+}
+
+func (m *AuthenticationManager) CountUnexpiredTokens(currentTime int) (ans int) {
+	for _, t := range m.mp {
+		if t+m.ttl > currentTime {
+			ans++
+		}
+	}
+	return
+}
