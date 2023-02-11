b80d496579e841ad1b2a563deda8103ea01aac85
Zhoutzzz <925920668@qq.com>
2023-02-11 12:53:19 +0800

    solution

---
+++ solution/easy/MinimumAmountFillCups_Lt2335.go
@@ -0,0 +1,11 @@
+package easy
+
+import "sort"
+
+func fillCups(amount []int) int {
+	sort.Ints(amount)
+	if amount[2] > amount[1]+amount[0] {
+		return amount[2]
+	}
+	return (amount[0] + amount[1] + amount[2] + 1) / 2
+}
