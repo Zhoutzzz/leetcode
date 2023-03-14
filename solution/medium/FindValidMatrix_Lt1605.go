f1c713532ef09448ddf9ee898f40ec4a629e1c56
Zhoutzzz <925920668@qq.com>
2023-03-14 20:48:54 +0800

    solution

---
+++ solution/medium/FindValidMatrix_Lt1605.go
@@ -0,0 +1,25 @@
+package medium
+
+import "leetcode/solution/common"
+
+func restoreMatrix(rowSum []int, colSum []int) [][]int {
+	n, m := len(rowSum), len(colSum)
+	matrix := make([][]int, n)
+	for i := range matrix {
+		matrix[i] = make([]int, m)
+	}
+	i, j := 0, 0
+	for i < n && j < m {
+		v := common.Min(rowSum[i], colSum[j])
+		matrix[i][j] = v
+		rowSum[i] -= v
+		colSum[j] -= v
+		if rowSum[i] == 0 {
+			i++
+		}
+		if colSum[j] == 0 {
+			j++
+		}
+	}
+	return matrix
+}
