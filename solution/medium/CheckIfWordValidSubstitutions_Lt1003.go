488199b3dfd279bdd422a6b3de52606b4079026e
Zhoutzzz <925920668@qq.com>
2023-05-03 21:46:17 +0800

    solution

---
+++ solution/medium/CheckIfWordValidSubstitutions_Lt1003.go
@@ -0,0 +1,12 @@
+package medium
+
+func isValid(s string) bool {
+	stk := []byte{}
+	for i, _ := range s {
+		stk = append(stk, s[i])
+		if len(stk) >= 3 && string(stk[len(stk)-3:]) == "abc" {
+			stk = stk[:len(stk)-3]
+		}
+	}
+	return len(stk) == 0
+}
