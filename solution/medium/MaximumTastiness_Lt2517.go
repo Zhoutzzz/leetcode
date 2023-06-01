28f1225828c4c37dc4d61cb2d44291ade9e1e966
Zhoutzzz <925920668@qq.com>
2023-06-01 21:34:29 +0800

    solution

---
+++ solution/medium/MaximumTastiness_Lt2517.go
@@ -0,0 +1,18 @@
+package medium
+
+import "sort"
+
+func maximumTastiness(price []int, k int) int {
+	sort.Ints(price)
+	return sort.Search((price[len(price)-1]-price[0])/(k-1), func(d int) bool {
+		d++ // 二分最小的 f(d+1) < k，从而知道最大的 f(d) >= k
+		cnt, pre := 1, price[0]
+		for _, p := range price[1:] {
+			if p >= pre+d {
+				cnt++
+				pre = p
+			}
+		}
+		return cnt < k
+	})
+}
