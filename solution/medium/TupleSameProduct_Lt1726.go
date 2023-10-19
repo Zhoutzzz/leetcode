ac40d214223d3ead783fe2ea154a76f89c916454
Zhoutzzz <925920668@qq.com>
2023-10-19 20:43:54 +0800

    solution

---
+++ solution/medium/TupleSameProduct_Lt1726.go
@@ -0,0 +1,16 @@
+package medium
+
+func tupleSameProduct(nums []int) int {
+	n := len(nums)
+	cnt := make(map[int]int)
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			cnt[nums[i]*nums[j]]++
+		}
+	}
+	ans := 0
+	for _, v := range cnt {
+		ans += v * (v - 1) * 4
+	}
+	return ans
+}
