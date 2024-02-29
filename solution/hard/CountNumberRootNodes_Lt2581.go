10e2c06305aa404b4660c4ac768999b208f8293e
Zhoutzzz <925920668@qq.com>
2024-02-29 08:58:53 +0800

    solution

---
+++ solution/hard/CountNumberRootNodes_Lt2581.go
@@ -0,0 +1,49 @@
+package hard
+
+func rootCount(edges [][]int, guesses [][]int, k int) int {
+	n := len(edges) + 1
+	g := make([][]int, n)
+	st := make(map[int64]int)
+
+	for _, v := range edges {
+		g[v[0]] = append(g[v[0]], v[1])
+		g[v[1]] = append(g[v[1]], v[0])
+	}
+	for _, v := range guesses {
+		st[h(v[0], v[1])] = 1
+	}
+
+	cnt, res := 0, 0
+	var dfs func(int, int)
+	dfs = func(x, fat int) {
+		for _, y := range g[x] {
+			if y == fat {
+				continue
+			}
+			if st[h(x, y)] == 1 {
+				cnt++
+			}
+			dfs(y, x)
+		}
+	}
+	dfs(0, -1)
+
+	var redfs func(int, int, int)
+	redfs = func(x, fat, cnt int) {
+		if cnt >= k {
+			res++
+		}
+		for _, y := range g[x] {
+			if y == fat {
+				continue
+			}
+			redfs(y, x, cnt-st[h(x, y)]+st[h(y, x)])
+		}
+	}
+	redfs(0, -1, cnt)
+	return res
+}
+
+func h(x, y int) int64 {
+	return int64(x)<<20 | int64(y)
+}
